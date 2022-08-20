89b40cda49e8356e550e6cb6e37fa07235d0001c
Braulio Diez Botella <braulio.diez@gmail.com>
2022-08-20 17:59:43 +0200

    global example

---
+++ 00-global/src/components/component-a.svelte
@@ -0,0 +1,14 @@
+<script template="ts">
+  import { onMount } from "svelte";
+  import {trackPromise} from '../common';
+  import { getApiA } from "../api";
+
+  let result = "";
+
+  onMount(() => {
+    trackPromise(getApiA().then((data) => (result = data)));
+  });
+</script>
+
+<h2>Component A</h2>
+<h3>result: {result}</h3>
---
+++ 00-global/src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as ComponentA } from "./component-a.svelte";
+export { default as ComponentB } from "./component-b.svelte";
